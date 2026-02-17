da7eed35c004329f3637a745a3d4cf9ffb2f4010
Simon Hartcher <simon@simonhartcher.com>
2026-02-17 17:19:45 +1100

    test: add validation for default values in basic and flag types

--- test/codegen/root.zig
+++ test/codegen/root.zig
@@ -3,5 +3,11 @@ test "call builtin method with void return type" {
     arr.set(0, .init(bool, true));
 }
 
+test "default values for basic and flag types" {
+    // Verify the opt struct with Dictionary default compiles (don't execute - needs valid surface data)
+    _ = &ArrayMesh.addSurfaceFromArrays;
+}
+
 const gdzig = @import("gdzig");
 const Array = gdzig.builtin.Array;
+const ArrayMesh = gdzig.class.ArrayMesh;
